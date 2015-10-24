a116025788981ad09f03ce100d0015e29c1639bd
JacobBurger <jmb23@rice.edu>
2015-10-24 17:12:21 -0500

    User inputted percentile
    Lol

--- my_app.py
+++ my_app.py
@@ -37,7 +37,7 @@ def percentile(meas):
     return stats*100
 
 def name_alter(name):
-    return percentile(300) + " testing data!"
+    return percentile(int(name)) + " testing data!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
