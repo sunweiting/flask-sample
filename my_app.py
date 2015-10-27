7a4e26b983155386a143cac3a3176f4d33f895b8
JacobBurger <jmb23@rice.edu>
2015-10-27 17:38:09 -0500

    fiddling with html

--- my_app.py
+++ my_app.py
@@ -27,8 +27,8 @@ def logout():
 	return redirect(url_for('index'))
 
 @app.route('/tool')
-def tool():
-	return "Hello World"
+def tool(template):
+	return ""
 
 def percentile(meas):
     mw_HE1S = open("Data/mw_HE1S.txt", "r")
